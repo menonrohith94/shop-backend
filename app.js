dab91e7ec0582fc90997a550fe2780f4c2c4d265
Rohith <menonrohith94@gmail.com>
2021-03-29 10:25:53 +0530

    Removed jade template. Added helper file.

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+
+
+/* CORS */
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
